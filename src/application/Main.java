c80f563d3cdbc123ea78a3a9656571afd6cd1734
Vitor <vitorvvc2005@gmail.com>
2023-10-24 14:33:13 -0300

    Melhoria na classe Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,77 +1,59 @@
 package application;
 
-import entities.Department;
-import entities.HourContract;
 import entities.Worker;
-import entitiesEnum.WorkerLevel;
 
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.Scanner;
+
+import static application.Util.*;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
         // Segue dicas para melhor leitura do código !!!
-        // Oque tiver ao lado de elementos como "Enter department's name: " // Design
-        // é uma sugestao sobre oque deve ser escrito para o teste do progama
+        // Oque tiver sem \\ ao final são sugestões sobre oque escrever para testar o progama
         // Comentários que iniciarem com // e acabarem com \\ são descricões sobre os métodos a seguir
 
-        // Formatacão de datas, para dia, mes e ano e somente para mes e ano \\
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("MM/yyyy");
+        // Lembrando que claro voce pode digitar oque quiser, desde que atenda os
+        // requisitos do programa, oque há em comentários são apenas conselhos \\
 
-        // Inicio do progama: \\
 
-        System.out.print("Enter department's name: "); // Design
-        String departament = sc.next();
-        sc.nextLine();
+        // Método que pede informacões sobre um usuário e o cria em Worker \\
+        System.out.println("========================");
+        System.out.println("Método userInfo");
+        System.out.println("========================");
+        Worker cnpjoto = userInfo();
+        // Departament: Design
+        // Name: Alex
+        // Level: mid_level
+        // BaseSalary: 1200.00
+        System.out.println("========================");
 
-        System.out.println("Enter worker data: ");
-        System.out.print("Name: "); // Alex
-        String name = sc.nextLine();
-        System.out.print("Level: "); // mid_level
-        WorkerLevel workerLevel = WorkerLevel.valueOf(sc.nextLine().toUpperCase());
-        System.out.print("Base salary: "); // 1200.00
-        double salary = sc.nextDouble();
-        sc.nextLine();
-        // Cria um trabalhador chamado cnpjoto com tudo que foi respondido pelo progama \\
-        Worker cnpjoto = new Worker(name, workerLevel, salary, new Department(departament));
 
-        // Pede um valor de quantos contratos serão adicionados, e referente ao valor respondido percorra o mesmo valor
-        // para adicionar contratos \\
-        System.out.print("How many contracts to this worker? "); // 3
-        int quantidade = sc.nextInt();
-        for (int i = 0; i < quantidade; i++) {
-            System.out.println("Enter contract #" + (i + 1) + " data: ");
-            sc.nextLine();
-            System.out.print("Date (DD/MM/YYYY): "); // 1: 20/08/2018 // 2: 13/06/2018 // 3: 25/08/2018
-            LocalDate date = LocalDate.parse(sc.nextLine(), dtf);
-            System.out.print("Value per Hour: "); // 1: 50.00 // 2: 30.00 // 3: 80.00
-            double contractSalary = sc.nextDouble();
-            System.out.print("Duration (hours): "); // 1: 20 // 2: 18.00 // 3: 10.00
-            int duration = sc.nextInt();
-            HourContract contract = new HourContract(date, contractSalary, duration);
-            // Adiciona aos contratos de cnpojoto ( worker ) o " contrato[i] " \\
-            cnpjoto.addContract(contract);
-        }
-        // Pede o mes que ele deseja saber seu faturamento, faturamento esse que é = salario base + salario de contratos
-        // daquele mes em especifico \\
-        sc.nextLine();
-        System.out.print("Enter month and year to calculate income (MM/YYYY): "); // 08/2018
-        // Recebe o mes e ano referentes e armazena cada um em uma variavel INT para mais precisão em outros métodos \\
-        YearMonth yearMonth = YearMonth.parse(sc.nextLine(), dtf2);
-        int monthValue = yearMonth.getMonthValue();
-        int year = yearMonth.getYear();
-        sc.close();
+        System.out.println("Método contractsInfo");
+        System.out.println("========================");
+        contractsInfo(cnpjoto);
+        // Contracts to add: 3
+        // Contract 1:
+        // Date: 20/08/2018
+        // Value Per Hour: 50.00
+        // Duration: 20
+        // Contract 2:
+        // Date: 13/06/2018
+        // Value Per Hour: 30.00
+        // Duration: 18
+        // Contract 3:
+        // Date: 25/08/2018
+        // Value Per Hour: 80.00
+        // Duration: 10
+        System.out.println("========================");
 
-        // Printa o Nome, Departamento & o quanto ele faturou no mes pedido anteriormente \\
-        System.out.println("Name: " + cnpjoto.getName());
-        System.out.println("Department: " + cnpjoto.getDepartament().getName());
-        System.out.println("Income for (" + yearMonth.format(dtf2) + "): " + String.format("%.2f", cnpjoto.income(year, monthValue)));
+        System.out.println("Método calcByThe");
+        System.out.println("========================");
+        calcByThe(cnpjoto);
+        // Enter monthh and year: 08/2018
+        System.out.println("========================");
+
+
     }
 }
