4b8ffd6173bbe6ce646e338bb192632baaa6b998
Vitor <vitorvvc2005@gmail.com>
2023-10-24 14:44:27 -0300

    Adicionando Comentários a classe Department

--- src/entities/Department.java
+++ src/entities/Department.java
@@ -2,12 +2,15 @@ package entities;
 
 public class Department {
 
+    // Classe departament que possue apenas um Nome como parametro
     private String name;
 
     public Department(String name) {
         this.name= name;
     }
 
+
+    // Get & Set
     public String getName() {
         return name;
     }
